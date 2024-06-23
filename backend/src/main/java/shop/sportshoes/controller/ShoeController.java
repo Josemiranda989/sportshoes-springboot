fd347bf608b8f92c57d363bd4cee2639f7efcaa9
EFFM00 <fischietto.elena@gmail.com>
2024-06-23 13:44:03 -0300

    se generan logs en consola

--- backend/src/main/java/shop/sportshoes/controller/ShoeController.java
+++ backend/src/main/java/shop/sportshoes/controller/ShoeController.java
@@ -1,9 +1,12 @@
 package shop.sportshoes.controller;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import shop.sportshoes.SportshoesApplication;
 import shop.sportshoes.model.Shoe;
 import shop.sportshoes.model.ShoeDto;
 import shop.sportshoes.repository.ShoeRepository;
@@ -20,8 +23,11 @@ public class ShoeController {
     @Autowired
     private ShoeService shoeService;
 
+    private static final Logger logger = LogManager.getLogger(SportshoesApplication.class);
+
     @GetMapping
     public ResponseEntity<?> getAllShoes() {
+        logger.info("Se obtienen todas las imagenes");
         try {
             List<Shoe> shoes = shoeService.getAllShoes();
             return ResponseEntity.ok(shoes);
@@ -32,6 +38,7 @@ public class ShoeController {
 
     @PostMapping
     public ResponseEntity<String> createShoe(@ModelAttribute ShoeDto shoeDto) {
+        logger.info("Se trata de guardar una imagen nueva");
         try {
             return ResponseEntity.ok(shoeService.saveImageStorage(shoeDto));
         } catch (Exception e) {
